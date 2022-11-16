2ba4e8c9493bba16e38f7ca22dd3c4be9480f967
Lewis-Ban <54550675+Lewis-Ban@users.noreply.github.com>
2022-11-15 22:53:33 -0800

    Update MainActivity.java

--- app/src/main/java/com/example/reminder_app/MainActivity.java
+++ app/src/main/java/com/example/reminder_app/MainActivity.java
@@ -8,6 +8,11 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText title, memo, date, time;
+    Button button;
+    String titleStr, memoStr, dateStr, timeStr;
+    SharedPreferences sp;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +22,32 @@ public class MainActivity extends AppCompatActivity {
     public void goToAddActivity(View view) {
         Intent intent = new Intent(this, AddActivity.class);
         startActivity(intent);
+        
+        
+        title = findViewById(R.id.textInputEditText2);
+        memo = findViewById(R.id.textInputEditText);
+        date = findViewById(R.id.editTextDate);
+        time = findViewById(R.id.editTextTime);
+        button = findViewById(R.id.button);
+
+        sp = getSharedPreferences("Pref", Context.MODE_PRIVATE);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                titleStr = title.getText().toString();
+                memoStr = memo.getText().toString();
+                dateStr = date.getText().toString();
+                timeStr = time.getText().toString();
+
+                SharedPreferences.Editor editor = sp.edit();
+                editor.putString("title", titleStr);
+                editor.putString("memo", memoStr);
+                editor.putString("date", dateStr);
+                editor.putString("time", timeStr);
+
+            }
+        });
     }
 
     public void goToReminderActivity(View view) {
